a4bf953ae13c19995fccdb3fb41f105417cce125
Jean-Baptiste-Lasselle <35227860+Jean-Baptiste-Lasselle@users.noreply.github.com>
2019-03-21 01:58:05 +0100

    Create build.waitaminute.js

---
+++ build.waitaminute.js
@@ -0,0 +1,19 @@
+# https://www.npmjs.com/package/wait-until
+var waitUntil = require('wait-until');
+var fs = require('fs');
+
+if (fs.existsSync(path)) {
+    // Do something
+}
+
+var resolutionDesDependancesTerminee = fs.existsSync("./public/assets/grapesjs/grapesjs.min.js") && fs.existsSync("./public/assets/grapesjs/grapesjs.min.css") && fs.existsSync("./public/assets/grapesjs/grapesjs.min.css") && fs.existsSync("./public/assets/grapesjs-mjml/grapesjs-mjml.min.js") && fs.existsSync("./public/assets/font-awesome/fonts/fontawesome-webfont.woff2")
+
+attendre () {
+  waitUntil(2, 7, function condition() {
+    return (  ? true : false);
+}, function done(result) {
+    console.log("");
+});
+}
+
+var fs = require('fs');
