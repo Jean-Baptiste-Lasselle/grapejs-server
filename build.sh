70c5a874f0d2423dbba7827053b46f2b65251c5f
Jean-Baptiste-Lasselle <35227860+Jean-Baptiste-Lasselle@users.noreply.github.com>
2019-03-21 01:40:36 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,14 +2,20 @@
 
 echo "Dependency Resolution"
 
+resolutionDesDependances () { 
 npm install
+}
+
+test () { 
+echo "No Tests to execute yet."
+}
 
 # Structure
 # ./public/assets/font-awesome/fonts
 # ./public/assets/grapesjs/grapesjs.min.js
 # ./public/assets/grapesjs/grapesjs.min.css
 # ./public/assets/grapesjs-mjml/grapesjs-mjml.min.js
-
+buildDuProjet () { 
 # Installing grapesjs inside client bundle
 cp node_modules/grapesjs/dist/grapesjs.min.js ./public/assets/grapesjs/grapes.min.js
 cp node_modules/grapesjs/dist/css/grapesjs.min.css ./public/assets/grapesjs/grapes.min.css
@@ -17,7 +23,7 @@ cp node_modules/grapesjs/dist/css/grapesjs.min.css ./public/assets/grapesjs/grap
 cp node_modules/grapesjs-mjml/dist/grapesjs-mjml.min.js ./public/assets/grapesjs-mjml/grapesjs-mjml.min.js
 # Installing fonts
 cp ./node_modules/font-awesome/fonts/fontawesome-webfont.woff2 ./public/assets/font-awesome/fonts/fontawesome-webfont.woff2
-
+                                                                 public/assets/font-awesome/fonts
 # 
 # Pour faire le bundle du client
 # Les [parcel index.html] et [parcel watch index.html] démarrent un serveur sur le port 1234
@@ -25,3 +31,6 @@ echo "Building : Client Bundle"
 # see. https://github.com/Jean-Baptiste-Lasselle/grapesjs-mjml-webclient/blob/620ead430419749662eecd783c170ed7b7e543dd/package.json#L8
 npm run bundle
 echo "Client Bundle Build successfully completed."
+}
+#le plus petit pipeline du monde
+resolutionDesDependances && test && buildDuProjet
