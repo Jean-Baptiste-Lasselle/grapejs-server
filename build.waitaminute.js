9f653526fa13e7e39435dddfe98f59796478a9ee
Jean-Baptiste-Lasselle <35227860+Jean-Baptiste-Lasselle@users.noreply.github.com>
2019-03-21 02:11:14 +0100

    Update build.waitaminute.js

--- build.waitaminute.js
+++ build.waitaminute.js
@@ -8,12 +8,9 @@ if (fs.existsSync(path)) {
 
 var resolutionDesDependancesTerminee = fs.existsSync("./node_modules/grapesjs/dist/grapesjs.min.js") && fs.existsSync("./node_modules/grapesjs/dist/css/grapesjs.min.css") && fs.existsSync("./node_modules/grapesjs-mjml/dist/grapesjs-mjml.min.js") && fs.existsSync("./node_modules/font-awesome/fonts/fontawesome-webfont.woff2")
 
-attendre () {
-  waitUntil(2, 7, function condition() {
+waitUntil(2, 7, function condition() {
     return ( resolutionDesDependancesTerminee ? true : false);
 }, function done(result) {
     console.log("La résolution des dépendances s'est terminée avec succès");
 });
-  
-}
 
