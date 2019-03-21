b2aa4c9ac0209dee409ec6bd03f33ae73ff1c977
Jean-Baptiste-Lasselle <35227860+Jean-Baptiste-Lasselle@users.noreply.github.com>
2019-03-21 02:14:29 +0100

    Update build.waitaminute.js

--- build.waitaminute.js
+++ build.waitaminute.js
@@ -3,7 +3,7 @@ var waitUntil = require('wait-until');
 var fs = require('fs');
 
 var resolutionDesDependancesTerminee = fs.existsSync("./node_modules/grapesjs/dist/grapesjs.min.js");
-resolutionDesDependancesTerminee = resolutionDesDependancesTerminee && fs.existsSync("./node_modules/grapesjs/dist/grapesjs.min.js);
+resolutionDesDependancesTerminee = resolutionDesDependancesTerminee && fs.existsSync("./node_modules/grapesjs/dist/grapesjs.min.js");
 resolutionDesDependancesTerminee = resolutionDesDependancesTerminee && fs.existsSync("./node_modules/font-awesome/fonts/fontawesome-webfont.woff2");
 
 waitUntil(2, 7, function condition() {
