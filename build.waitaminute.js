5f9f434c9b55d414c5b95b9be1526207e76c97cf
Jean-Baptiste-Lasselle <35227860+Jean-Baptiste-Lasselle@users.noreply.github.com>
2019-03-21 02:02:05 +0100

    Update build.waitaminute.js

--- build.waitaminute.js
+++ build.waitaminute.js
@@ -6,14 +6,14 @@ if (fs.existsSync(path)) {
     // Do something
 }
 
-var resolutionDesDependancesTerminee = fs.existsSync("./public/assets/grapesjs/grapesjs.min.js") && fs.existsSync("./public/assets/grapesjs/grapesjs.min.css") && fs.existsSync("./public/assets/grapesjs/grapesjs.min.css") && fs.existsSync("./public/assets/grapesjs-mjml/grapesjs-mjml.min.js") && fs.existsSync("./public/assets/font-awesome/fonts/fontawesome-webfont.woff2")
+var resolutionDesDependancesTerminee = fs.existsSync("./node_modules/grapesjs/dist/grapesjs.min.js") && fs.existsSync("./node_modules/grapesjs/dist/css/grapesjs.min.css") && fs.existsSync("./node_modules/grapesjs-mjml/dist/grapesjs-mjml.min.js") && fs.existsSync("./node_modules/font-awesome/fonts/fontawesome-webfont.woff2")
 
 attendre () {
   waitUntil(2, 7, function condition() {
-    return (  ? true : false);
+    return ( resolutionDesDependancesTerminee ? true : false);
 }, function done(result) {
-    console.log("");
+    console.log("La résolution des dépendances s'est terminée avec succès");
 });
+  
 }
 
-var fs = require('fs');
